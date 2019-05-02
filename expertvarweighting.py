e6988ea9ddcbaca17d272e16326977235879816c
pdrembelas <pdrembelas@users.noreply.github.com>
2019-05-02 09:10:27 -0700

    Update expertvarweighting.py

--- expertvarweighting.py
+++ expertvarweighting.py
@@ -1,12 +1,15 @@
 """
 This code creates an optimizer thats weights inputs from VAR model and WSJ
 """
-
 import pandas as pd
 
-wsj_data = pd.read_csv('/Users/vibhor/Desktop/Models/TimeSeriesModels/wsj_mev.csv', index_col=0, parse_dates=True)
-df_mean_mev = wsj_data[['mev_type','forecast_date','date','values']].groupby(['date','forecast_date','mev_type']).mean().unstack()
-df_median_mev = wsj_data[['mev_type','forecast_date','date','values']].groupby(['date','forecast_date','mev_type']).median().unstack()
-#wsj_data_wide = wsj_data.pivot(values='values', columns=['forecast_date'])
 
-df_median_mev.sort_values(by=['date','forecast_date'],inplace=True)
+wsj_data = pd.read_csv('wsj_mev.csv', index_col=0, parse_dates=['date', 'forecast_date'])
+
+df_mean_mev = wsj_data[['mev_type', 'forecast_date', 'date', 'values']]\
+    .groupby(['date', 'forecast_date', 'mev_type']).mean().reset_index()
+
+df_median_mev = wsj_data[['mev_type', 'forecast_date', 'date', 'values']]\
+    .groupby(['date', 'forecast_date', 'mev_type']).median().reset_index()
+
+df_median_mev.sort_values(by=['date', 'forecast_date'], inplace=True)
